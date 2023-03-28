927d891a141311a7bd87eb3f849996e3a929b983
Will Blair <85643015+willblair0708@users.noreply.github.com>
2023-03-28 00:39:39 -0400

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themeum/tutor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2ddb9f6f22cdfa6b8f0c14c8a407ff9fa7b14115',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'themeum/tutor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2ddb9f6f22cdfa6b8f0c14c8a407ff9fa7b14115',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
